2c88ab1e5e5a4db7754ad213676f1c90b0dcd695
anura123644 <abc.priyadarsini@gmail.com>
2025-02-20 18:52:32 +0530

    Add files via upload

---
+++ lesson.2.py
@@ -0,0 +1,12 @@
+#Let's check the datatypes of different values
+a=1
+
+b=1.0
+
+c="coding"
+
+d=False
+print("Data type of" ,type (a) )
+print("Data type of" ,type (b) )
+print("Data type of" ,type (c) )
+print("Data type of" ,type (d) )
