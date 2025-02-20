4adf37d617d231ead7c4c03fc811d0f75875c43b
anura123644 <abc.priyadarsini@gmail.com>
2025-02-20 18:50:02 +0530

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+#Print a Senntence
+print("welcome to the world of coding")
+#Print a Number
+print(4)
+#Next Line after print
+print("hello python\n")
+#Pass multiple arguments for printing
+print("hello",5)
+#End argument of print statement
+print("welcome to",end="*")
