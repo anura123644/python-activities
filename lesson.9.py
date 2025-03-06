99cf5d64b3bcc2ba5ff19d9f6a94ae907563d90e
anura123644 <abc.priyadarsini@gmail.com>
2025-03-06 18:51:21 +0530

    Add files via upload

---
+++ lesson.9.py
@@ -0,0 +1,11 @@
+import math
+
+def calculate_circumference(radius):
+    circumference = 2 * math.pi * radius
+    return circumference
+
+# Example usage:
+radius = 5
+circumference = calculate_circumference(radius)
+print(f"The circumference of the circle with radius {radius} is {circumference}")
+11
