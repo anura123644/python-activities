f483f658afb19ee490557b5104a97adcc4c729c6
anura123644 <abc.priyadarsini@gmail.com>
2025-03-13 18:30:11 +0530

    Add files via upload

---
+++ lesson.19.py
@@ -0,0 +1,10 @@
+# Program to check voting eligibility
+
+# Input: Age of the person
+age = int(input("Enter your age: "))
+
+# Conditional statement to check eligibility
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote yet.")
