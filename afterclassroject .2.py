fdee766159eb69f9cb709054cbe3b26a17b084db
anura123644 <abc.priyadarsini@gmail.com>
2025-03-14 18:26:37 +0530

    Add files via upload

---
+++ afterclassroject .2.py
@@ -0,0 +1,10 @@
+# Program to check if a number is even or odd
+
+# Input the number from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"{number} is an Even number.")
+else:
+    print(f"{number} is an Odd number.")
