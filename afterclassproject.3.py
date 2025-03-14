ad2385bf190c3ed85bcdbc90e5672cae1dc1acd3
anura123644 <abc.priyadarsini@gmail.com>
2025-03-14 18:26:52 +0530

    Add files via upload

---
+++ afterclassproject.3.py
@@ -0,0 +1,13 @@
+# Storing birthday dates in variables
+birthday1 = "1990-01-15"
+birthday2 = "1985-05-23"
+birthday3 = "2000-12-07"
+birthday4 = "1995-07-30"
+birthday5 = "1998-11-11"
+
+# Printing the stored birthday dates
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
