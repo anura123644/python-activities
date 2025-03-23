4469b968b90c9daaece963ac44b44b525a6d19cc
anura123644 <abc.priyadarsini@gmail.com>
2025-03-23 12:39:05 +0530

    Add files via upload

---
+++ symetric difference.py
@@ -0,0 +1,9 @@
+# Define two sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+# Find the symmetric difference
+symmetric_difference = set1.symmetric_difference(set2)
+
+# Print the result
+print("Symmetric Difference:", symmetric_difference)
