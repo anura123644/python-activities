dd96370a978ef8ff78c191f072df47aed6551a47
anura123644 <abc.priyadarsini@gmail.com>
2025-05-17 17:44:04 +0530

    Add files via upload

---
+++ afterl2.py
@@ -0,0 +1,10 @@
+# Define a tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Convert tuple to list
+my_list = list(my_tuple)
+
+# Print the result
+print("Tuple:", my_tuple)
+print("Converted List:", my_list)
+
